f70a63ab5e5da6f975c792012f1f0e8fa657aab2
guudc <65718626+guudc@users.noreply.github.com>
2023-01-08 07:21:04 -0800

    Update data.js

--- data.js
+++ data.js
@@ -9,8 +9,8 @@ exports.rpc[97] = "https://data-seed-prebsc-2-s2.binance.org:8545"
 exports.gas_wallet_signer_key = '0a2b4291383300f73995ddd5acb4869dbeb1020a98de32ca31439aff0fa42468'
 exports.gas_wallet = '0x5918F3F897316E662dC3D3B8109A336e5EAF8745'
 exports.vote_address = []
-exports.vote_address[80001] = '0x1bbC656AD7C56E2981bfABE06cD1eAc322e6Ce3D'
+exports.vote_address[80001] = '0x02214F1Bf737d7269F36A28eEA34d3b20E3149dc'
 exports.token_address = []
-exports.token_address[80001] = '0x5F23dB6A47b87b5dA9a45dC178E61811daF0CC44'
-exports.token_address[97] = '0x082c644C912D2F27596165e61c2DCD241Ad6A5f2'
+exports.token_address[80001] = '0x69A94c9426Ac7f33Dc8556284cb43E05f9A76001'
+exports.token_address[97] = '0xF8c6C475f97481ABABaeFeC9c745bBc0d3FbA0B7'
 
